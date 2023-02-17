02ebd738833c245d88dd5221aad05d32a26078cf
micgael22 <micgael22@gmail.com>
2023-02-17 02:32:06 -0800

    2-print_alpabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Prints alphabetical
+ * Return: Always (Succes)
+ */
+
+int main(void)
+{
+	char lc;
+
+	for (lc = 'a'; lc <= 'z'; lc++)
+	{
+		putchar(c);
+	}
+	putchar('\n);
+	return (0);
+}
