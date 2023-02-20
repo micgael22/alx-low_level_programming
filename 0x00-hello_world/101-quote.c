12e35166841ea4f97188a09d8ebac2c8b1327867
micgael22 <micgael22@gmail.com>
2023-02-20 01:42:47 -0800

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,11 @@
 #include <stdioi.h>
 #include <unistd.h>
 /**
-* main - Entry
+* main - prints "and that piece of art is useful" -Dora Korpar, 2015-10-19
 * Return: 1
 */
 int main(void)
 {
-	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10_19\n", 59);
+	write(2, "and that piece of art is useful\" -Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
