1c2cc51d2e68f1ff7217e426330bbb7adc342c37
micgael22 <micgael22@gmail.com>
2023-02-22 00:25:16 -0800

    100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -14,7 +14,7 @@ void print_times_table(int n)
 	for (num = 0; num <= n; num++)
 	{
 	_putchar('0');
-	for (mult = 1; mult <=  n; mult++)
+	for (mult = 1; mult <= n; mult++)
 	{
 	_putchar(',');
 	_putchar(' ');
@@ -26,7 +26,7 @@ void print_times_table(int n)
 	if (prod >= 100)
 	{
 	_putchar((prod / 100) + '0');
-	_putchar(((prod / 10)) + '0');
+	_putchar(((prod / 10)) % 10 + '0');
 	}
 	else if (prod <= 99 && prod >= 10)
 	{
