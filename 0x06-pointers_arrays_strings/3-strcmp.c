36786a6f98f712562e006b309e771c14f6269cca
micgael22 <micgael22@gmail.com>
2023-03-02 04:24:18 -0800

    3-strcmp.c

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -9,7 +9,7 @@
 
 int _strcmp(char *s1, char *s2)
 {
-	int 1;
+	int i;
 
 	i = 0;
 	while (s1[i] != '\0' && s2[i] != '\0')
@@ -18,7 +18,7 @@ int _strcmp(char *s1, char *s2)
 		{
 			return (s1[i] - s2[i]);
 		}
-	i++;
+		i++;
 	}
 	return (0);
 }
